ef02d22d0a474e726f13c31ecbd4ddeb1eac713f
Ivan <ivankaminskii323@gmail.com>
2022-12-27 17:01:55 +0100

    service refactoring

--- src/main/java/com/netflix/sandBox/exception/ApiError.java
+++ src/main/java/com/netflix/sandBox/exception/ApiError.java
@@ -1,17 +1,12 @@
 package com.netflix.sandBox.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-@Setter
 public class ApiError {
-    private String message;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String debugMessage;
-
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<String> errors;
 
@@ -19,17 +14,14 @@ public class ApiError {
         this.message = message;
         this.debugMessage = debugMessage;
     }
+
     public ApiError(String message) {
-        this.message=message;
+        this.message = message;
     }
 
     public ApiError(String message, String debugMessage, List<String> errors) {
         this.message = message;
         this.debugMessage = debugMessage;
         this.errors = errors;
-    }
-
-    public ApiError() {
-
     }
 }
